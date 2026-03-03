7789298256b452f6db10f5dc4947061b9d22b1f5
TomasHegr <hegr.tomik@seznam.cz>
2026-03-03 11:24:37 +0100

    Nahrání

--- src/main.c
+++ src/main.c
@@ -1,62 +1,41 @@
+#include "main.h"
+#include "daughterboard.h"
+#include "milis.h"
 #include <stdbool.h>
 #include <stm8s.h>
-#include "main.h"
-#include "milis.h"
-//#include "delay.h"
-//#include "uart1.h"
-//#include <stdio.h>
+// #include "delay.h"
+// #include "uart1.h"
+#include <stdio.h>
 
-// Discovery Board
-#ifdef STM8S003
-#define LED_PORT GPIOD
-#define LED_PIN  GPIO_PIN_0
-#define BTN_PORT GPIOB
-#define BTN_PIN  GPIO_PIN_7
-#endif
-// Dero Board
-#ifdef STM8S103
-#define LED_PORT GPIOD
-#define LED_PIN  GPIO_PIN_4
-#endif
-// Discovery Board
-#ifdef STM8S105
-#define LED_PORT GPIOD
-#define LED_PIN  GPIO_PIN_0
-#endif
-// Nucleo Kit
-#ifdef STM8S208
-#define LED_PORT GPIOC
-#define LED_PIN  GPIO_PIN_5
-#define BTN_PORT GPIOE
-#define BTN_PIN  GPIO_PIN_4
-#endif
+
 
 
 void init(void)
 {
-    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1); // taktovani MCU na 16MHz
     init_milis();
-    //init_uart1();
+    // init_uart1();
 
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-
+    GPIO_Init(DB_LED6_PORT, DB_LED6_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(DB_LED5_PORT, DB_LED5_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(DB_S1_PORT, DB_S1_PIN, GPIO_MODE_IN_PU_NO_IT);
 }
 
 
 int main(void)
 {
-  
+
     uint32_t time = 0;
 
     init();
 
     while (1) {
-        if (milis() - time > 333 ) {
-            REVERSE(LED); 
+        if (milis() - time > 333) {
+            REVERSE(DB_LED6);
             time = milis();
-            //printf("%ld\n", time);
+            // printf("%ld\n", time);
         }
-        //delay_ms(333);
+        // delay_ms(333);
     }
 }
 
